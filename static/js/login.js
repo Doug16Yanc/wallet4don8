14fe03966a6fdd12f94f1e9f4136546706a60455
Douglas <douglasholanda3195@gmail.com>
2025-03-21 20:57:23 -0300

    Mais páginas html

--- static/js/login.js
+++ static/js/login.js
@@ -3,3 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "index.html";
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("reset_password").addEventListener("click", function () {
+        window.location.href = "reset_password.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("create_user").addEventListener("click", function () {
+        window.location.href = "create_user.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("do_login").addEventListener("click", function () {
+        window.location.href = "causes.html"
+    });
+});
+
